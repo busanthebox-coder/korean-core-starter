e85a8745bc7db229e0000e27f026ad403c07b217
hannam <hannam@hannamui-MacBookPro.local>
2026-07-28 18:00:01 +0900

    Updates

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_VERSION = 'kcs-pwa-4659412ed816';
+const CACHE_VERSION = 'kcs-pwa-03d323b3d37d';
 const PRECACHE_URLS = [
   ".",
   "404.html",
   "assets/App-BBEFnfd-.css",
-  "assets/App-Xlu5U6J_.js",
-  "assets/index-B2NvnbFz.js",
+  "assets/App-Cd6AitUf.js",
+  "assets/index-BmfFjD0f.js",
   "assets/index-Cd1Exfud.css",
   "assets/vendor-IyN17Iu6.js",
   "data/app-core.b01172e1.json",
